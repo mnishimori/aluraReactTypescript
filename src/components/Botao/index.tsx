ca0d19e1195ed1af7af19769c5ca1920d2310921
mnishimori <mnishimori@yahoo.com.br>
2024-09-22 19:20:40 -0300

    feat(aluraStudies): formulário, lista, botão, cronômetro.

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import style from './Botao.module.scss';
+import style from "./Botao.module.scss";
 
-class Botao extends React.Component {
-    render() {
-        return (
-            <button className={style.botao}>Botão</button>
-        )
-    }
+interface IProps {
+    children: React.ReactNode;
+}
+
+class Botao extends React.Component<IProps> {
+  render() {
+    return <button className={style.botao}>{this.props.children}</button>;
+  }
 }
 
 export default Botao;
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,30 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Lista.module.scss";
+import Item from "./Item";
 
 function Lista() {
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'Javascript',
-        tempo: '01:00:00'
-    }, {
-        tarefa: 'Typescript',
-        tempo: '03:00:00'
-    }]
-    return (
-        <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
-            <ul>
-                {tarefas.map((item, index) => 
-                    <li key={index} className={style.item}>
-                        <h3>{item.tarefa}</h3>
-                        <span>{item.tempo}</span>
-                    </li>
-                )}
-            </ul>
-        </aside>
-    )
+  const [tarefas, setTarefas] = useState([
+    {
+      tarefa: "React",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "Javascript",
+      tempo: "01:00:00",
+    },
+    {
+      tarefa: "Typescript",
+      tempo: "03:00:00",
+    },
+  ]);
+  return (
+    <aside className={style.listaTarefas}>
+      <h2
+        onClick={() => {
+          setTarefas([
+            ...tarefas,
+            { tarefa: "Estudar estado", tempo: "05:00:00" },
+          ]);
+        }}
+      >
+        Estudos do dia
+      </h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <Item key={index} {...item} />
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 export default Lista;
